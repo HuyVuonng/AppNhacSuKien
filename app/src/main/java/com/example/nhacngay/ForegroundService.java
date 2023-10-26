65f6f66d4ebdfba0539600fc3e6c7ae298a26fcc
HuyVuong <vuonghuy12345@gmail.com>
2023-10-26 15:10:03 +0700

    change time repeat

--- app/src/main/java/com/example/nhacngay/ForegroundService.java
+++ app/src/main/java/com/example/nhacngay/ForegroundService.java
@@ -258,7 +258,7 @@ public class ForegroundService extends Service {
                             }
 
                             try {
-                                Thread.sleep(90000);
+                                Thread.sleep(900000);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                                 Bitmap bitmap = BitmapFactory.decodeResource(ForegroundService.this.getResources(), R.mipmap.ic_launcherevent);
